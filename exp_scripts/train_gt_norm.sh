b2bead3874d1a2dbb029e9839dc44ea2b692af88
eric <cgx1997@outlook.com>
2022-04-26 14:07:14 -0700

    save model every 500k steps

--- exp_scripts/train_gt_norm.sh
+++ exp_scripts/train_gt_norm.sh
@@ -10,4 +10,4 @@ num_steps="$3"
 dataset_absdirpath=$(echo "$ldif_repo_dirpath/dataset_GT_norm_$shape_name/")
 experiment_name=$(echo "GT_norm_$shape_name")
 
-python train.py --dataset_directory $dataset_absdirpath --experiment_name $experiment_name --model_type ldif --train_step_count $num_steps --checkpoint_interval 5000
+python train.py --dataset_directory $dataset_absdirpath --experiment_name $experiment_name --model_type ldif --train_step_count $num_steps --checkpoint_interval 500000
