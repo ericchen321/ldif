862c2259a22cc954f76e17c3d46727217bf20421
eric <cgx1997@outlook.com>
2022-05-24 00:00:04 -0700

    removed batch size; set ckpt interval to 1k

--- exp_scripts/train_gt_norm.sh
+++ exp_scripts/train_gt_norm.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # usage:
-#   ./train_gt_norm.sh <absolute path to ldif/> <shape name> <batch_size> <num steps>
+#   ./train_gt_norm.sh <absolute path to ldif/> <shape name> <num steps>
 
 ldif_repo_dirpath="$1"
 shape_name="$2"
-batch_size="$3"
-num_steps="$4"
+num_steps="$3"
 
 # define paths and experiment name
 dataset_absdirpath=$(echo "$ldif_repo_dirpath/dataset_$shape_name/")
@@ -15,6 +14,5 @@ python train.py \
 --dataset_directory $dataset_absdirpath \
 --experiment_name $experiment_name \
 --model_type ldif \
---batch_size $batch_size \
 --train_step_count $num_steps \
---checkpoint_interval 500000
+--checkpoint_interval 1000
