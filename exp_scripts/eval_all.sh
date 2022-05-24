7077d802c39f9cc5bb706932f6af724615952671
eric <cgx1997@outlook.com>
2022-05-24 13:09:59 -0700

    merged batch eval scripts because can't parallelize

--- exp_scripts/eval_all.sh
+++ exp_scripts/eval_all.sh
@@ -10,3 +10,9 @@ resolution="$2"
 ./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath bimba $resolution
 ./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath camera $resolution
 ./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath david $resolution
+./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath dragon $resolution
+./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath dragon_warrior $resolution
+./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath engine $resolution
+./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath guangong $resolution
+./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath lion $resolution
+./exp_scripts/eval_gt_norm.sh $ldif_repo_dirpath thai_statue $resolution
